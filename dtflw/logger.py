f728150e8775375d21b8dd0b1edca3273a874593
Can Ozil <ozil@soleynet.soley.io>
2023-01-04 00:26:19 +0100

    [Feature] [COZ] Add verbose parameter to LoggerBase

--- dtflw/logger.py
+++ dtflw/logger.py
@@ -19,9 +19,16 @@ class DefaultLogger(LoggerBase):
     """
     A flow default logger which logs to stdout using `print`.
     """
-
+    def __init__(self):
+        self.verbosity = "default"
+        self.info = False
+        
     def log(self, msg: str = ""):
         """
         Logs a message.
         """
-        print(msg)
+        assert self.verbosity == "verbose" or self.verbosity == "default", f"Verbosity variable: \"{self.verbosity}\" must be set either \"verbose\" or \"default\""
+        assert isinstance(self.info, bool) , f"Info variable: \"{self.info}\" must be boolean!"
+
+        if self.verbosity == "verbose" or self.info:
+            print(msg)
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -5,15 +5,94 @@ from io import StringIO
 
 
 class DefaultLoggerTestCase(unittest.TestCase):
+        
+        
+    def test_default_logger_log_in_default_verbosity(self):
 
-    def log(self):
-
-        logger = DefaultLogger()
+        default_logger = DefaultLogger()
 
         out = StringIO()
         sys.stdout = out
-        logger.log('hello world!')
+
+        default_logger.log('hello world!')
+        output = out.getvalue().strip()
+
+        # Assert
+        self.assertEqual(output, "")
+
+
+    def test_default_logger_log_in_verbose_verbosity(self):
+
+        default_logger = DefaultLogger()
+
+        out = StringIO()
+        sys.stdout = out
+        default_logger.verbosity="verbose"
+
+        default_logger.log('hello world!')
         output = out.getvalue().strip()
 
         # Assert
         self.assertEqual(output, "hello world!")
+
+                
+    def test_default_logger_log_in_true_info(self):
+
+        default_logger = DefaultLogger()
+
+        out = StringIO()
+        sys.stdout = out
+        default_logger.info = True
+
+        default_logger.log('hello world!')
+        output = out.getvalue().strip()
+
+        # Assert
+        self.assertEqual(output, "hello world!")
+
+                      
+    def test_default_logger_log_in_false_info(self):
+
+        default_logger = DefaultLogger()
+
+        out = StringIO()
+        sys.stdout = out
+        default_logger.info = False
+
+        default_logger.log('hello world!')
+        output = out.getvalue().strip()
+
+        # Assert
+        self.assertEqual(output, "")
+                      
+
+    def test_default_logger_log_fails_wrong_verbosity(self):
+
+        default_logger = DefaultLogger()
+
+        out = StringIO()
+        sys.stdout = out
+        default_logger.verbosity = "False"
+
+        # Assert
+        with self.assertRaisesRegex(AssertionError, f"Verbosity variable: \"{default_logger.verbosity}\" must be set either \"verbose\" or \"default\""):
+            
+            default_logger.log('hello world!')
+            output = out.getvalue().strip()
+
+
+    def test_default_logger_log_fails_wrong_info(self):
+
+        default_logger = DefaultLogger()
+
+        out = StringIO()
+        sys.stdout = out
+        default_logger.info = "String"
+
+        # Assert
+        with self.assertRaisesRegex(AssertionError, f"Info variable: \"{default_logger.info}\" must be boolean!"):
+            
+            default_logger.log('hello world!')
+            output = out.getvalue().strip()
+
+     
