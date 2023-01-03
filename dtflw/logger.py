2a0b1ec2f4ee5ec7030bd87db7d9fc305b6f55ff
Can Ozil <ozil@soleynet.soley.io>
2023-01-04 00:26:19 +0100

    [Feature] [COZ] Add verbose parameter to LoggerBase

--- dtflw/logger.py
+++ dtflw/logger.py
@@ -15,9 +15,16 @@ class DefaultLogger(LoggerBase):
     """
     A flow default logger which logs to stdout using `print`.
     """
-
+    def __init__(self):
+        self.verbosity = "default"
+        self.info = False
+        
     def log(self, msg: str = ""):
         """
         Method which will log a message.
         """
-        print(msg)
+        assert self.verbosity == "verbose" or self.verbosity == "default", f"Verbosity variable: \"{self.verbosity}\" must be set either \"verbose\" or \"default\""
+        assert isinstance(self.info, bool) , f"Info variable: \"{self.info}\" must be boolean!"
+
+        if self.verbosity == "verbose" or self.info:
+            print(msg)
