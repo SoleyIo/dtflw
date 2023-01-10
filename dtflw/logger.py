00a02f862ec1ef40adaf25f67f1182ae5334c044
Can Ozil <ozil@soleynet.soley.io>
2023-01-10 14:17:14 +0100

    [Feature] Update

--- dtflw/logger.py
+++ dtflw/logger.py
@@ -17,12 +17,14 @@ class DefaultLogger(LoggerBase):
     """
     def __init__(self):
         self.verbosity = "default"
-
+        self.info = False
+        
     def log(self, msg: str = ""):
         """
         Method which will log a message.
         """
         assert self.verbosity == "verbose" or self.verbosity == "default", f"Verbosity variable: \"{self.verbosity}\" must be set either \"verbose\" or \"default\""
+        assert isinstance(self.info, bool) , f"Info variable: \"{self.info}\" must be boolean!"
 
-        if self.verbosity == "verbose":
+        if self.verbosity == "verbose" or self.info:
             print(msg)
