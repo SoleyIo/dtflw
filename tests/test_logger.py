7245fd25ff2bb342e83d12ba8eca1f64b8e49d1a
Can Ozil <ozil@soleynet.soley.io>
2023-01-10 13:27:16 +0100

    [Feature] Simpler the logic

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -34,36 +34,6 @@ class DefaultLoggerTestCase(unittest.TestCase):
 
         # Assert
         self.assertEqual(output, "hello world!")
-
-                
-    def test_default_logger_log_in_true_info(self):
-
-        default_logger = DefaultLogger()
-
-        out = StringIO()
-        sys.stdout = out
-        default_logger.info = True
-
-        default_logger.log('hello world!')
-        output = out.getvalue().strip()
-
-        # Assert
-        self.assertEqual(output, "hello world!")
-
-                      
-    def test_default_logger_log_in_false_info(self):
-
-        default_logger = DefaultLogger()
-
-        out = StringIO()
-        sys.stdout = out
-        default_logger.info = False
-
-        default_logger.log('hello world!')
-        output = out.getvalue().strip()
-
-        # Assert
-        self.assertEqual(output, "")
                       
 
     def test_default_logger_log_fails_wrong_verbosity(self):
@@ -76,37 +46,5 @@ class DefaultLoggerTestCase(unittest.TestCase):
 
         # Assert
         with self.assertRaisesRegex(AssertionError, f"Verbosity variable: \"{default_logger.verbosity}\" must be set either \"verbose\" or \"default\""):
-            
             default_logger.log('hello world!')
-            output = out.getvalue().strip()
-
-
-    def test_default_logger_log_fails_wrong_info(self):
-
-        default_logger = DefaultLogger()
-
-        out = StringIO()
-        sys.stdout = out
-        default_logger.info = "String"
-
-        # Assert
-        with self.assertRaisesRegex(AssertionError, f"Info variable: \"{default_logger.info}\" must be boolean!"):
-            
-            default_logger.log('hello world!')
-            output = out.getvalue().strip()
-
-
-    def test_default_logger_log_default_verbosity_true_info(self):
-
-        default_logger = DefaultLogger()
-
-        out = StringIO()
-        sys.stdout = out
-        default_logger.info = True
-
-        default_logger.log('hello world!')
-        output = out.getvalue().strip()
-
-        # Assert
-        self.assertEqual(output, "hello world!")
      
