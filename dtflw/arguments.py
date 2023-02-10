7d6a79d777d198775ccfb73306e90b0ad3ab7b47
golovatenko@soleynet.soley.io <golovatenko@SoleyLaptop-57.local>
2023-02-10 22:23:09 +0100

    Added arguments.py. WIP.

---
+++ dtflw/arguments.py
@@ -0,0 +1,80 @@
+import databricks as db
+
+class Arg:
+
+    NAME_SUFFIX = ""
+
+    def __init__(self, name, value):
+        self.__name = name
+        self.__value = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def value(self):
+        return self.__value
+
+    @property
+    def hasValue(self):
+        return True
+
+    def __repr__(self):
+        return self.value
+
+    @classmethod
+    def _create_widget(cls, name, value, widget_name_suffix):
+        widget_name = f"{name}{widget_name_suffix}"
+        # If a widget already exists then the line below has not effect.
+        dbutils = db.get_dbutils()
+        dbutils.widgets.text(widget_name, str(value), widget_name)
+
+        return name, dbutils.widgets.get(widget_name)
+
+    @classmethod
+    def create(cls, name, value):
+        return cls(*cls._create_widget(name, value, cls.NAME_SUFFIX))
+
+
+class Input(Arg):
+    NAME_SUFFIX = "_in"
+
+    @property
+    def hasValue(self):
+        return len(self.value) > 0
+
+
+class Output(Arg):
+    NAME_SUFFIX = "_out"
+
+    @property
+    def hasValue(self):
+        return len(self.value) > 0
+
+
+def unpack_inial_values(values):
+
+    if len(values) == 1 and isinstance(values[0], dict):
+        return values[0]
+    else:
+        # TODO: get values from the bus.
+        # TODO: if a value is not found (None) then raise an error.
+        return {name: None for name in values}
+
+
+def initialize_arguments(c, *values):
+    names_and_values = unpack_inial_values(values)
+    return {name: c.create(name, value) for name, value in names_and_values.items()}
+
+
+def init_args(*values):
+    return initialize_arguments(Arg, *values)
+
+
+def init_inputs(*values):
+    return initialize_arguments(Input, *values)
+
+
+def init_outputs(*values):
+    return initialize_arguments(Output, *values)
