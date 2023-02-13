86e49d0aa612fe37d0e5adeaee84502afdfd23bb
skndrg <henrypuh@gmail.com>
2023-02-13 15:54:11 +0100

    Added unit tests.

--- dtflw/arguments.py
+++ dtflw/arguments.py
@@ -2,23 +2,37 @@ import dtflw.databricks as db
 
 
 class Arg:
+    """
+    An argument passed to a notebook.
+    """
 
     NAME_SUFFIX = ""
 
     def __init__(self, name, value):
+        if name is None or len(name) == 0:
+            raise ValueError("'name' cannot be None nor an empty string.")
         self.__name = name
+
+        if value is None or type(value) != str:
+            raise ValueError("'value' cannot be None and must be a string.")
         self.__value = value
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @property
-    def value(self):
+    def value(self) -> str:
+        """
+        Value is always of type string.
+        """
         return self.__value
 
     @property
-    def hasValue(self):
+    def has_value(self):
+        """
+        Always True for Arg.
+        """
         return True
 
     def __repr__(self):
@@ -35,26 +49,44 @@ class Arg:
 
     @classmethod
     def create(cls, name, value):
+        """
+        Factory method.
+        """
         return cls(*cls._create_widget(name, value, cls.NAME_SUFFIX))
 
 
 class Input(Arg):
+    """
+    An input table required by a notebook.
+    """
     NAME_SUFFIX = "_in"
 
     @property
-    def hasValue(self):
+    def has_value(self):
+        """
+        Returns False if value is an empty string. Otherwise True.
+        """
         return len(self.value) > 0
 
 
 class Output(Arg):
+    """
+    An output table promissed by a notebook.
+    """
     NAME_SUFFIX = "_out"
 
     @property
-    def hasValue(self):
+    def has_value(self):
+        """
+        Returns False if value is an empty string. Otherwise True.
+        """
         return len(self.value) > 0
 
 
-def initialize_arguments(c, *values):
+def initialize_arguments(argument_class, *values):
+    """
+    Initializes widgets using `dbutils.widgets.text`. Parameter `argument_calss` can be `Arg`, `Input` or `Output`.
+    """
 
     names_and_values = {}
     if len(values) == 1 and isinstance(values[0], dict):
@@ -62,16 +94,79 @@ def initialize_arguments(c, *values):
     else:
         names_and_values = {name: "" for name in values}
 
-    return {name: c.create(name, value) for name, value in names_and_values.items()}
+    return {name: argument_class.create(name, value) for name, value in names_and_values.items()}
 
 
-def init_args(*values):
-    return initialize_arguments(Arg, *values)
+def init_args(*args):
+    """
+    Initializes widgets using `dbutils.widgets.text` for args:
+    ```
+    flow.notebook(...)
+        .args(...)
+    ```
+
+    Parameters
+    ----------
+    *args
+        Option 1:
+            Given as a variable number of non-keyword arguments, e.g. `args = init_args("a", "b", "c")`.
+            All the args will have an empty string as a value.
+        Option 2:
+            Given as a dictionary with default values, e.g. `args = init_args({'a': 42})`.
+            All non-string values will be stringified.
+
+    Returns
+    -------
+    dict[str: Arg]
+    """
+    return initialize_arguments(Arg, *args)
 
 
-def init_inputs(*values):
-    return initialize_arguments(Input, *values)
+def init_inputs(*inputs):
+    """
+    Initializes widgets using `dbutils.widgets.text` for input tables:
+    ```
+    flow.notebook(...)
+        .input('Orders')
+    ```
+
+    Parameters
+    ----------
+    *inputs
+        Option 1:
+            Given as a variable number of non-keyword arguments, e.g. `inputs = init_inputs("Orders", "Customers")`.
+            All the inputs will have an empty string as a value.
+        Option 2:
+            Given as a dictionary with default values, e.g. `inputs = init_inputs({'Orders': 'orders.parquet'})`.
+            All non-string values will be stringified.
+
+    Returns
+    -------
+    dict[str: Input]
+    """
+    return initialize_arguments(Input, *inputs)
 
 
 def init_outputs(*values):
+    """
+    Initializes widgets using `dbutils.widgets.text` for output tables:
+    ```
+    flow.notebook(...)
+        .output('Orders')
+    ```
+
+    Parameters
+    ----------
+    *args
+        Option 1:
+            Given as a variable number of non-keyword arguments, e.g. `outputs = init_outputs("Orders", "Customers")`.
+            All the args will have an empty string as a value.
+        Option 2:
+            Given as a dictionary with default values, e.g. `outputs = init_outputs({'Orders': 'orders.parquet'})`.
+            All non-string values will be stringified.
+
+    Returns
+    -------
+    dict[str: Output]
+    """
     return initialize_arguments(Output, *values)
--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -1,14 +1,49 @@
 import unittest
-from dtflw.arguments import init_args, init_inputs, init_outputs
+from dtflw.arguments import initialize_arguments, Arg, Input, Output
+from unittest.mock import patch
+import tests.utils as utils
+from ddt import ddt, data, unpack
 
 
+@ddt
 class ArgumentsTestCase(unittest.TestCase):
 
-    def test_init_args_names(self):
+    @data(
+        (Arg, True),
+        (Input, False),
+        (Output, False)
+    )
+    @unpack
+    @patch("dtflw.databricks.get_dbutils")
+    def test_initialize_arguments_no_values(self, clazz, expected_has_value, get_dbutils_mock):
 
-        actual_args = init_args("foo", "bar")
+        get_dbutils_mock.return_value = utils.DButilsMock()
+
+        actual_args = initialize_arguments(clazz, "foo")
 
         self.assertDictEqual(
-            actual_args,
-            {"foo": "", "bar": ""}
+            {a.name: a.value for name, a in actual_args.items()},
+            {"foo": ""}
+        )
+
+        self.assertEqual(actual_args["foo"].has_value, expected_has_value)
+
+    @data(Arg, Input, Output)
+    @patch("dtflw.databricks.get_dbutils")
+    def test_initialize_arguments_non_empty_values(self, clazz, get_dbutils_mock):
+
+        get_dbutils_mock.return_value = utils.DButilsMock()
+
+        actual_args = initialize_arguments(
+            clazz,
+            {"a": "data.parquet", "b": 42, "c": None, "d": True}
+        )
+
+        self.assertDictEqual(
+            {a.name: a.value for name, a in actual_args.items()},
+            {"a": "data.parquet", "b": "42", "c": "None", "d": "True"}
+        )
+
+        self.assertTrue(
+            all([a.has_value for a in actual_args.values()])
         )
