35b89ef2e827cbd29e1bf3789cdda285ccd4129a
skndrg <henrypuh@gmail.com>
2023-02-24 10:51:13 +0100

    Moved to src dir.

--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description_content = f.read()
+
 setup(
     name="dtflw",
     version="0.5.0",
     author="Soley GmbH",
-    author_email="",
-    long_description="dtflw is a Python framework for building modular data pipelines based on Databricks dbutils.notebook API.",
+    author_email="codeofconduct@soley.io",
+
     description="dtflw is a Python framework for building modular data pipelines based on Databricks dbutils.notebook API.",
+
+    long_description=long_description_content,
+    long_description_content_type="text/markdown",
+
     license="BSD 3-Clause License",
+
     url="https://github.com/SoleyIo/dtflw",
-    packages=find_packages(
-        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+
+    packages=find_packages("src"),
+
     classifiers=[
         "Programming Language :: Python :: 3.*",
         "License :: BSD 3-Clause License"
     ],
+
     install_requires=[
         "ddt>=1.5.0",
         "databricks-connect",
