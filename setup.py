246bfc196a47b653e5150ccf9077cba5d3722fc6
skndrg <henrypuh@gmail.com>
2023-02-24 12:43:01 +0100

    Moved to src.

--- setup.py
+++ setup.py
@@ -31,7 +31,8 @@ setup(
 
     keywords="databricks, data pipelines, etl, data engineering",
 
-    packages=find_packages(where="dtflw"),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
 
     python_requires=">=3.8",
 
