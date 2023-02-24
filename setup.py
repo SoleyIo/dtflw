af5f5568cbaa14fa96f230bd92e9fb0a9587c913
skndrg <henrypuh@gmail.com>
2023-02-24 12:14:55 +0100

    setup upd

--- setup.py
+++ setup.py
@@ -1,33 +1,48 @@
 from setuptools import setup, find_packages
+import pathlib
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description_content = f.read()
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description_content = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="dtflw",
     version="0.5.0",
-    author="Soley GmbH",
-    author_email="codeofconduct@soley.io",
 
     description="dtflw is a Python framework for building modular data pipelines based on Databricks dbutils.notebook API.",
-
     long_description=long_description_content,
     long_description_content_type="text/markdown",
 
-    license="BSD 3-Clause License",
-
     url="https://github.com/SoleyIo/dtflw",
 
-    packages=find_packages("src"),
+    author="Soley GmbH",
+    author_email="codeofconduct@soley.io",
 
     classifiers=[
-        "Programming Language :: Python :: 3.*",
-        "License :: BSD 3-Clause License"
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Databricks Data Engineers",
+        "Topic :: Software Development :: Data Pipelines",
+        "License :: OSI Approved :: BSD License :: 3-Clause 'New' or 'Revised' License (BSD-3-Clause)",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
     ],
+
+    keywords="databricks, data pipelines, etl, data engineering"
+
+    packages=find_packages(where="dtflw"),
+
+    python_requires=">=3.8",
 
     install_requires=[
         "ddt>=1.5.0",
         "databricks-connect",
         "setuptools"
     ],
+
+    project_urls={
+        "Bug Reports": "https://github.com/SoleyIo/dtflw/issues",
+        "Source": "https://github.com/SoleyIo/dtflw"
+    },
 )
