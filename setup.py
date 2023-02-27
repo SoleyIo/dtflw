ac4ab0d350a2c43c7aa25fa27f837e76f0c6174c
skndrg <henrypuh@gmail.com>
2023-02-27 10:45:29 +0100

    lond desc upd

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 from setuptools import setup, find_packages
-import pathlib
 
-here = pathlib.Path(__file__).parent.resolve()
-
-# Get the long description from the README file
-long_description_content = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="dtflw",
     version="0.5.1",
 
     description="dtflw is a Python framework for building modular data pipelines based on Databricks dbutils.notebook API.",
-    long_description=long_description_content,
+    long_description="See [the home page](https://github.com/SoleyIo/dtflw/blob/main/README.md) of the project for details.",
     long_description_content_type="text/markdown",
 
     url="https://github.com/SoleyIo/dtflw",
